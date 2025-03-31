9228f6297d9d458f4d0cd16d3a492a83e58d5737
Sofiia Pereima <pereima.pn@ucu.edu.ua>
2025-03-31 12:30:05 +0300

    функція для отримання елемента за індексом

---
+++ get node/get_nth.py
@@ -0,0 +1,17 @@
+class Node(object):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+        
+def get_nth(node, index):
+    if index < 0:
+        raise Exception('Error occured')
+    ptr = node
+    i = 0
+    while ptr is not None and i < index:
+        ptr = ptr.next
+        i += 1
+    if ptr is None:
+        raise Exception('Error occured')
+    if ptr is not None:
+        return ptr.data
