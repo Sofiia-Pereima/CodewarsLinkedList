941c0ad7abaf5e53fd12e20b299b88f03ecbe3cb
Sofiia Pereima <pereima.pn@ucu.edu.ua>
2025-03-31 22:26:49 +0300

    функція для видалення дублікатів

--- remove duplicates/remove_duplicates.py
+++ remove duplicates/remove_duplicates.py
@@ -3,28 +3,6 @@ class Node(object):
         self.data = data
         self.next = None
 
-# def push(head, data):
-#     ptr = Node(data)
-#     ptr.next = head
-#     return ptr
-
-# def stringify(node):
-#     ptr = node
-#     result = ''
-#     while ptr is not None:
-#         result += str(ptr.data) + ' -> '
-#         ptr = ptr.next
-#     result += 'None'
-#     return result
-
-# def build_list(lst):
-#     head = Node(None)
-#     ptr = head
-#     for el in lst:
-#         ptr.next = Node(el)
-#         ptr = ptr.next
-#     return head.next
-
 def remove_duplicates(head):
     if head is None:
         return head
@@ -39,8 +17,3 @@ def remove_duplicates(head):
     if ptr.data != ptr1.data:
         ptr1.next = Node(ptr.data)
     return head1.next
-
-# t = build_list([1, 2, 3, 3, 4, 4, 5])
-# print(stringify(t))
-# p = remove_duplicates(build_list([1, 1, 1]))
-# print(stringify(p))
